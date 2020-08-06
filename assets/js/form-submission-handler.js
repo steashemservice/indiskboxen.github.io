63c1c92a402eeecc277ea05de2ac03fe695e3578
steashemservice <32518087+steashemservice@users.noreply.github.com>
2020-08-07 01:14:31 +0200

    Update form-submission-handler.js

--- assets/js/form-submission-handler.js
+++ assets/js/form-submission-handler.js
@@ -130,7 +130,7 @@
     var colls = document.querySelectorAll("button.collapsible");
     for (var i = 0; i < colls.length; i++) {
       colls[i].addEventListener("click", function() {
-        //this.classList.toggle("active");
+        this.classList.toggle("active");
         var content = this.nextElementSibling;
         if (content.style.display === "block") {
           content.style.display = "none";
