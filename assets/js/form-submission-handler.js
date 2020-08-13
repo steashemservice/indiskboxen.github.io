9befdca10e2a13bdcbcd8d1d1a688cc7ed5fe863
steashemservice <32518087+steashemservice@users.noreply.github.com>
2020-08-13 22:16:25 +0200

    Update form-submission-handler.js

--- assets/js/form-submission-handler.js
+++ assets/js/form-submission-handler.js
@@ -3,7 +3,7 @@
   function getFormData(form) {
     var elements = form.elements;
     var d = new Date();
-    var ordernum = {}:
+    var ordernum = {};
     ordernum.name = 'order';
     ordernum.value = d.getFullYear().toString().slice(-2)+('0'+(d.getMonth()+1)).slice(-2)+('0'+d.getDate()).slice(-2)+('0'+d.getHours()).slice(-2)+('0'+d.getMinutes()).slice(-2)+('0'+d.getSeconds()).slice(-2);
     elements.unshift(ordernum);
