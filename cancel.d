57ec85e87214577e8806e09757f19b509f30402c
steashemservice <32518087+steashemservice@users.noreply.github.com>
2020-09-16 16:46:14 +0200

    Create cancel.d

---
+++ cancel.d
@@ -0,0 +1,29 @@
+---
+layout: page
+title: AVSLUTAR
+description: Vill du pausar ditt abonnemang kontaktar du oss via mejl på info@indiskaboxen.se så pausar vi ditt abonnemang under den period du önskar.
+image: 
+nav-menu: true
+published: true
+permalink: avslutar
+order: 999
+---
+
+<section id="contact">
+	<div class="inner">
+		<section>
+		    <header class="major">
+          <h2>Avslutar Abonnemang</h2>
+        </header>
+		    <form id="cancel-form" class="contact-form" method="post" data-success="ditt begäran om avslutning av prenumeration har registrerats!">
+  			  <input type="hidden" name="_next" value="thanks" />
+			    <input id="dispnon" name="_prev" type="text" <div class="field">
+          <div class="field">    
+  			    <label for="email">E-mail*</label>
+  			    <input id="email" type="email" name="email" class="field" required />
+			    </div>
+			    <button type="submit">Skicka Beställ</button>
+		    </form>
+		</section>
+  </div>
+</section>
