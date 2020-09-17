c535707327e147d1eff93b1b3446f3136a8397da
steashemservice <32518087+steashemservice@users.noreply.github.com>
2020-09-17 20:04:43 +0200

    Update form-submission-handler.js

--- assets/js/form-submission-handler.js
+++ assets/js/form-submission-handler.js
@@ -220,7 +220,7 @@
     for (var i = 0; i < colls.length; i++) {
       $(colls[i]).on("change paste keyup", function() {
         var content = $(this).parent().siblings('.cblock');
-        var pcode = $(this).val();
+        var pcode = $(this).val().split(" ").join("");
         var pmsg = $(this).siblings('label.collapsible');
         if(pcode.length == 5 && checkPcode(Number(pcode),plists)) {
           pmsg.html("Ja, vi levererar i ditt område!");
