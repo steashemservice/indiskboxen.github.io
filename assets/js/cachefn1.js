2de01ddbc236555ba2724cbfcdf1b071841b94bb
steashemservice <32518087+steashemservice@users.noreply.github.com>
2020-09-29 12:49:24 +0200

    Update cachefn1.js

--- assets/js/cachefn1.js
+++ assets/js/cachefn1.js
@@ -1,8 +1,12 @@
 (function($) {
   function jsonToURI(json){ return encodeURIComponent(JSON.stringify(json)); }
   function uriToJSON(urijson){ return JSON.parse(decodeURIComponent(urijson)); }
-  var datamsg = {"amount":{"value":1000,"editable":false},"message":{"value":"TEST","editable":false},"payee":{"value":"123 342 54 44","editable":false},"version":1};
-  var dataenc = jsonToURI(datamsg);
-  var req = "swish://payment?data="+dataenc;
-  $('a.swish-link').attr('href',req);
+  var swishFunc = function(event){
+    var $button = $(this);
+    var datamsg = {"amount":{"value":1000,"editable":false},"message":{"value":"TEST","editable":false},"payee":{"value":"123 342 54 44","editable":false},"version":1};
+    var dataenc = jsonToURI(datamsg);
+    var req = "swish://payment?data="+dataenc;
+    window.location.replace(req);
+  }
+  $(".swish-link").on("click", swishFunc);
 })(jQuery);
