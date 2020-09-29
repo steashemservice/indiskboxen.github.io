30c2808f06c1fdbc351d9844420a426355ff3884
steashemservice <32518087+steashemservice@users.noreply.github.com>
2020-09-29 14:36:59 +0200

    Update cachefn1.js

--- assets/js/cachefn1.js
+++ assets/js/cachefn1.js
@@ -6,9 +6,9 @@
     var datamsg = {"amount":{"value":1000,"editable":false},"message":{"value":"TEST","editable":false},"payee":{"value":"123 342 54 44","editable":false},"version":1};
     var dataenc = jsonToURI(datamsg);
     var req = "swish://payment?data="+dataenc;
-    timestart = new Date().getTime();
-    createIFrame(req+"&browser="+browserName+"&back="+encodeURIComponent(location.toString())+"&useragent="+encodeURIComponent(userAgent));
-    timeend = new Date().getTime();
+    var timestart = new Date().getTime();
+    //createIFrame(req+"&browser="+browserName+"&back="+encodeURIComponent(location.toString())+"&useragent="+encodeURIComponent(userAgent));
+    var timeend = new Date().getTime();
     if(timeend - timestart > 3000) {
       window.location.replace(req);
     } else {
