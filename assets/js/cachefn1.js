e1c083064aff5d9731656d5ff04725e6552bd3da
steashemservice <32518087+steashemservice@users.noreply.github.com>
2020-09-30 16:19:19 +0200

    Update cachefn1.js

--- assets/js/cachefn1.js
+++ assets/js/cachefn1.js
@@ -7,8 +7,8 @@
   }
   var swishFunc = function(event){
     var $button = $(this);
-    var total = $('.ordertotal').eq(0).text().replace(/\"/g, "");
-    var order = $('.orderid').eq(0).text().replace(/\"/g, "");
+    var total = $('.ordertotal').eq(0).text();
+    var order = $('.orderid').eq(0).text();
     var datamsg = {"amount":{"value":eval(total),"editable":false},"message":{"value":eval(order),"editable":false},"payee":{"value":"123 342 54 44","editable":false},"version":1};
     var dataenc = jsonToURI(datamsg);
     alert(dataenc);
