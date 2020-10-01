b15e22599350bc3efc150bf918fd485fe1b102cf
steashemservice <32518087+steashemservice@users.noreply.github.com>
2020-10-01 16:21:33 +0200

    Update cachefn1.js

--- assets/js/cachefn1.js
+++ assets/js/cachefn1.js
@@ -9,6 +9,32 @@
   }
   $(function() {
     GetURLParameter("res");
-    window.location.replace("https://www.indiskaboxen.se/thanks.html");
+    var formData = {};
+    formData["order"] = 'TEST';
+    formData["total"] = '1000';
+    formData["status"] = 'pass';
+    formData.formGoogleSendEmail = 'dilipfeb@gmail.com';
+    // url encode form data for sending as post data
+    var encoded = Object.keys(formData).map(function(k) {
+        return encodeURIComponent(k) + "=" + encodeURIComponent(formData[k]);
+    }).join('&');
+    var url = 'https://script.google.com/macros/s/AKfycbxTFQx9GwgAnlKzOPmvTV1Oiib_dTD_fEgnkxT1fhHI2DDIGb4/exec';
+    $.ajax({
+      url: url,
+      data: encoded,
+      type: "POST",
+      dataType: "json",
+      statusCode: {
+        404: function() {
+          window.location.replace("https://www.indiskaboxen.se/fail.html");
+        },
+        500: function() {
+          window.location.replace("https://www.indiskaboxen.se/fail.html");
+        },
+        200: function() {
+          window.location.replace("https://www.indiskaboxen.se/thanks.html");
+        }
+      }
+    });
   });
 })(jQuery);
