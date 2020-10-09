373476571923d16f77b468bf5917cd78985bfa61
steashemservice <32518087+steashemservice@users.noreply.github.com>
2020-10-09 11:40:19 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -308,5 +308,17 @@
   		$('#contact-form input[name=subscription]').prop("checked", true); 
 	});
   } );
-
+  function showSlides() {
+    var slideIndex = 0;
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+  };
+  document.addEventListener("DOMContentLoaded", showSlides, false);  
 })(jQuery);
