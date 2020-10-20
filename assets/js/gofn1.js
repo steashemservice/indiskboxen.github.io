eb5aba9326c9919e48bb94ce87801ce48bd8a04c
steashemservice <32518087+steashemservice@users.noreply.github.com>
2020-10-20 23:28:40 +0200

    Update gofn1.js

--- assets/js/gofn1.js
+++ assets/js/gofn1.js
@@ -12,7 +12,8 @@
       var amount = urlParams.get('amount');
     }
     var slink = swishgen(amount,order,addr);
-    //elements['swish'].value="swish://payment?data="+preq(elements['total'].value,elements['order'].value)+"&callbackurl="+encodeURIComponent("https://www.indiskaboxen.se/postcall?email="+elements['email'].value+"&order="+elements['order'].value+"&total="+elements['total'].value)+"&callbackresultparameter=res";
     $('#swish-uri').text(slink);
+    $('.orderid').text(order);
+    $('.ordertotal').text(amount);
   });
 })(jQuery);
