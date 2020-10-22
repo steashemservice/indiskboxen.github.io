f8f707fe1bbd057316fe0d8f142d25aa2ab425ad
steashemservice <32518087+steashemservice@users.noreply.github.com>
2020-10-22 23:28:20 +0200

    Update form-submission-handler.js

--- assets/js/form-submission-handler.js
+++ assets/js/form-submission-handler.js
@@ -21,6 +21,54 @@ function swishgen(total,order,addr) {
   }
   $(".swish-link").on("click", swishFunc);
   // get all data in form and return object
+  function getCancelFormData(form) {
+    var elements = form.elements;
+    var d = new Date();
+    var trap,honeypot;
+    var fields = Object.keys(elements).filter(function(k) {
+      if (elements[k].name === "_prev") {
+        honeypot = elements[k].value;
+        return false;
+      } else if (elements[k].name === "_next") {
+        trap = elements[k].value;
+        return false;
+      } else if (elements[k].value === "0") {
+        return false;
+      }
+      return true;
+    }).map(function(k) {
+      if(elements[k].name !== undefined) {
+        return elements[k].name;
+      // special case for Edge's html collection
+      }else if(elements[k].length > 0){
+        return elements[k].item(0).name;
+      }
+    }).filter(function(item, pos, self) {
+      return self.indexOf(item) == pos && item;
+    });
+    var formData = {};
+    fields.forEach(function(name){
+      var element = elements[name];
+      // singular form elements just have one value
+      formData[name] = element.value;
+      // when our element has multiple items, get their values
+      if (element.length) {
+        var data = [];
+        for (var i = 0; i < element.length; i++) {
+          var item = element.item(i);
+          if (item.checked || item.selected) {
+            data.push(item.value);
+          }
+        }
+        formData[name] = data.join(', ');
+      }
+    });
+    // add form-specific values into the data
+    formData.formDataNameOrder = JSON.stringify(fields);
+    formData.formGoogleSheetName = form.dataset.sheet || "order"; // default sheet name
+    formData.formGoogleSendEmail = form.dataset.email || ""; // no email by default
+    return {data: formData, honeypot: honeypot, trap:trap};
+  }
   function getFormData(form) {
     var elements = form.elements;
     var d = new Date();
@@ -198,13 +246,11 @@ function swishgen(total,order,addr) {
     var form = event.target,
         form_e = $( "#cancel-form" ),
         notice = form_e.find('#notice');
-    var formData = getFormData(form);
+    var formData = getCancelFormData(form);
     if (formData.honeypot || formData.trap !== 'thanks') {
       return false;
     }
     var data = formData.data;
-    delete data.order;
-    delete data.swish;
     // url encode form data for sending as post data
     var encoded = Object.keys(data).map(function(k) {
         return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
