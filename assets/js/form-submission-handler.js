bbae1f90af0d044d3fc30d5da9586b7d5a870d62
steashemservice <32518087+steashemservice@users.noreply.github.com>
2020-11-17 12:56:51 +0100

    Update form-submission-handler.js

--- assets/js/form-submission-handler.js
+++ assets/js/form-submission-handler.js
@@ -358,7 +358,7 @@ function swishgen(total,order,addr) {
 (function($) {
   function get_delivery_date() {
     var date = new Date();
-    date.setDate(date.getDate()+4);
+    date.setDate(date.getDate()+6);
     while(date.getDay() !== 0) {
       date.setDate(date.getDate()+1);
     }
