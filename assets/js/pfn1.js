4139d199e3088bf2c75688ab34df077be61297b5
steashemservice <32518087+steashemservice@users.noreply.github.com>
2021-02-25 20:02:00 +0100

    Update pfn1.js

--- assets/js/pfn1.js
+++ assets/js/pfn1.js
@@ -1,7 +1,7 @@
 (function($) {
   var myFrame = document.getElementById('gFrame');
   const loc = window.location.search;//Get the search string of the domain
-  window.history.replaceState(null, null, window.location.pathname);
+  //window.history.replaceState(null, null, window.location.pathname);
   const urlParams = new URLSearchParams(loc);
   if (urlParams.has('name') && urlParams.has('email') && urlParams.has('amount') && urlParams.has('order')) {
     var frameSrc = myFrame.src.toString();
