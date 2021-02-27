2e452ccb96e41309b35218042afee97349f12c61
steashemservice <32518087+steashemservice@users.noreply.github.com>
2021-02-27 16:17:44 +0100

    Update pfn1.js

--- assets/js/pfn1.js
+++ assets/js/pfn1.js
@@ -6,7 +6,7 @@
   if (urlParams.has('name') && urlParams.has('email') && urlParams.has('amount') && urlParams.has('order')) {
     var frameSrc = myFrame.src.toString();
     myFrame.src = frameSrc + loc;
-    var slink = swishgen(urlParams.get('amount'),urlParams.get('order'),urlParams.get('email'));
+    var slink = swishgen(Number(urlParams.get('amount'))/100,urlParams.get('order'),urlParams.get('email'));
     $('#swish-uri').text(slink);
     $('.orderid').text(urlParams.get('order'));
     $('.ordertotal').text(Number(urlParams.get('amount'))/100);
