7e3631cc54d1edce74bb575df8761abdd7e48479
steashemservice <32518087+steashemservice@users.noreply.github.com>
2021-02-28 14:20:13 +0100

    Update form-submission-handler.js

--- assets/js/form-submission-handler.js
+++ assets/js/form-submission-handler.js
@@ -1,7 +1,7 @@
 function jsonToURI(json){ return encodeURIComponent(JSON.stringify(json)); }
 function uriToJSON(urijson){ return JSON.parse(decodeURIComponent(urijson)); }
 function preq(t,o) {
-  var datamsg = {"amount":{"value":eval(t),"editable":true},"message":{"value":o,"editable":false},"payee":{"value":"123 478 05 32","editable":false},"version":1};
+  var datamsg = {"amount":{"value":eval(t),"editable":false},"message":{"value":o,"editable":false},"payee":{"value":"123 478 05 32","editable":false},"version":1};
   var dataenc = jsonToURI(datamsg);
   return dataenc;
 }
